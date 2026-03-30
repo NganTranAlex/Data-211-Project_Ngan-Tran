4dcd1893a872ae952c0e573c03632e252f828a8d
NganTranAlex <tranthanhngan.2906.alex@gmail.com>
2026-03-30 12:43:13 -0600

    load csv file & fill in missing values for total_bedrooms column

--- California House Price Prediction.py
+++ California House Price Prediction.py
@@ -4,3 +4,9 @@ from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 
+# load the dataset/ csv file
+california_house_prices = pd.read_csv("C:/Codefile/housing file/housing.csv")
+
+# fill in the missing values (the missing values are usually present in total_bedrooms)
+california_house_prices["total_bedrooms"] = california_house_prices["total_bedrooms"].fillna(california_house_prices["total_bedrooms"].median())
+
